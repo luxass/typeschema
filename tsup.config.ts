b91d3902c14b2fab760cc317adc1a3f6fd204398
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-10-11 20:30:32 +0200

    bump deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,9 @@ import { version } from './package.json';
 const workers = ['src/workers/json-schema-worker.ts', 'src/workers/zod-schema-worker.ts'];
 const entry = ['src/cli.ts', 'src/index.ts', 'src/config.ts', 'src/esbuild-plugin.ts'];
 
+// We do this, because cli.d.ts will have duplicate lines of #!/bin/env node
+// Should probably report this - will need to find where it's coming from.
+// This is a workaround for now.
 const dtsEntry = entry.concat(workers).filter((file) => file !== 'src/cli.ts');
 
 export default defineConfig({
@@ -12,8 +15,6 @@ export default defineConfig({
   format: ['esm'],
   clean: true,
   dts: {
-    // We do this, because cli.d.ts will have duplicate lines of #!/bin/env node
-    // Should probably report this - will need to find where it's coming from.
     entry: dtsEntry
   },
   external: ['typescript'],
