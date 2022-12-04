2d30066105e0e52a172e670fd6f5400336f2f91f
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-12-04 22:21:02 +0100

    started on tests

---
+++ test/plugins/object-plugin.ts
@@ -0,0 +1,10 @@
+import ts from 'typescript';
+
+import { TypeSchemaPlugin } from '../../src';
+
+export const MyTypeScriptPlugin: TypeSchemaPlugin = {
+  name: 'my-typescript-plugin',
+  setup(ctx) {
+
+  }
+};
